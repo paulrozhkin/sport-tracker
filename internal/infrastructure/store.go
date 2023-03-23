6d36a78ed0fb7ae58ab157f48817b25ff1103df0
paulrozhkin <blackiiifox@gmail.com>
2023-03-23 23:00:00 +0300

    use pgxpool

--- internal/infrastructure/store.go
+++ internal/infrastructure/store.go
@@ -1,50 +1,76 @@
 package infrastructure
 
 import (
-	"database/sql"
+	"context"
 	"errors"
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/lib/pq"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/paulrozhkin/sport-tracker/config"
+	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
 
 type Store struct {
-	Db *sql.DB
+	Pool *pgxpool.Pool
 }
 
-func NewPostgresStore(cfg *config.Configuration,
+func NewPostgresStore(lc fx.Lifecycle,
+	cfg *config.Configuration,
 	logger *zap.SugaredLogger) (*Store, error) {
 	// TODO: заменить на pgxpool
 	database := cfg.Database
 	psqlInfo := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s",
 		database.DBUser, database.DBPassword, database.DBConnection, database.DBName, database.DBSslMode)
-	db, err := sql.Open("postgres",
-		psqlInfo)
+	err := migrateDatabase("file:///Data/Projects/sport-tracker/data/migrations", psqlInfo, logger)
 	if err != nil {
+		logger.Error("Failed to create migrator due to", zap.Error(err))
 		return nil, err
 	}
-	err = db.Ping()
+	store := new(Store)
+	logger.Info("Database connected")
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			pool, connectErr := pgxpool.New(ctx, psqlInfo)
+			if connectErr != nil {
+				logger.Error("Failed to connect to database", zap.Error(connectErr))
+				return connectErr
+			}
+			pingError := pool.Ping(ctx)
+			if pingError != nil {
+				logger.Error("Failed to ping database", zap.Error(pingError))
+				return err
+			}
+			store.Pool = pool
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			if store.Pool != nil {
+				store.Pool.Close()
+			}
+			return nil
+		},
+	})
+	return store, nil
+}
+
+func migrateDatabase(pathToMigrations, connectionUrl string, logger *zap.SugaredLogger) error {
+	m, err := migrate.New(
+		pathToMigrations,
+		connectionUrl)
 	if err != nil {
-		return nil, err
-	}
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
-	if err != nil {
-		return nil, err
-	}
-	m, err := migrate.NewWithDatabaseInstance(
-		"file:///Data/Projects/sport-tracker/data/migrations",
-		database.DBName, driver)
-	if err != nil {
-		return nil, err
+		return err
 	}
 	err = m.Up()
-	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		return nil, err
+	if err != nil {
+		if !errors.Is(err, migrate.ErrNoChange) {
+			return err
+		}
+		logger.Info("Database no need migrations")
+	} else {
+		logger.Info("Database migrated to new version")
 	}
-	logger.Info("Database connected")
-	return &Store{Db: db}, nil
+	return nil
 }
--- internal/repositories/users_repository.go
+++ internal/repositories/users_repository.go
@@ -1,8 +1,10 @@
 package repositories
 
 import (
+	"context"
 	"database/sql"
 	"errors"
+	"github.com/jackc/pgx/v5"
 	"github.com/paulrozhkin/sport-tracker/internal/infrastructure"
 	"github.com/paulrozhkin/sport-tracker/internal/models"
 	"go.uber.org/zap"
@@ -22,7 +24,7 @@ func (ur *UsersRepository) CreateUser(user models.User) (*models.User, error) {
 	user.FillForCreate()
 	query := `INSERT INTO users (id, created, updated, username, password, name, gender, height) 
 				VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
-	_, err := ur.store.Db.Exec(query, user.Id, user.Created, user.Updated,
+	_, err := ur.store.Pool.Exec(context.Background(), query, user.Id, user.Created, user.Updated,
 		user.Username, user.Password, user.Name, user.Gender, user.Height)
 	if err != nil {
 		ur.log.Error("Failed to create user", err)
@@ -31,9 +33,9 @@ func (ur *UsersRepository) CreateUser(user models.User) (*models.User, error) {
 	return ur.GetUserById(user.Id)
 }
 func (ur *UsersRepository) GetUserById(id string) (*models.User, error) {
-	query := `SELECT (id, created, updated, username, password, name, gender, height) 
+	query := `SELECT id, created, updated, username, password, name, gender, height
 				FROM users WHERE id=$1`
-	row := ur.store.Db.QueryRow(query, id)
+	row := ur.store.Pool.QueryRow(context.Background(), query, id)
 	user, err := rowToUser(row)
 	if err != nil && errors.Is(sql.ErrNoRows, err) {
 		return nil, models.NewNotFoundByIdError("user", id)
@@ -45,8 +47,8 @@ func (ur *UsersRepository) GetUserById(id string) (*models.User, error) {
 }
 
 func (ur *UsersRepository) GetUserByUsername(username string) (*models.User, error) {
-	query := "SELECT * FROM users WHERE username=$1"
-	row := ur.store.Db.QueryRow(query, username)
+	query := "SELECT id, created, updated, username, password, name, gender, height  FROM users WHERE username=$1"
+	row := ur.store.Pool.QueryRow(context.Background(), query, username)
 	user, err := rowToUser(row)
 	if err != nil && errors.Is(sql.ErrNoRows, err) {
 		return nil, models.NewNotFoundError("user", username, "username")
@@ -57,9 +59,8 @@ func (ur *UsersRepository) GetUserByUsername(username string) (*models.User, err
 	return user, nil
 }
 
-func rowToUser(row *sql.Row) (*models.User, error) {
+func rowToUser(row pgx.Row) (*models.User, error) {
 	user := &models.User{}
-
 	err := row.Scan(&user.Id, &user.Created,
 		&user.Updated, &user.Username,
 		&user.Password, &user.Name, &user.Gender,
